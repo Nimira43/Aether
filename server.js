5a50b3e5bd45a0a1aca8bf305c405a82be45adb8
Nimira43 <n.rabone72@aol.com>
2024-12-09 17:16:26 +0000

    Setting up CORS

--- server.js
+++ server.js
@@ -2,3 +2,15 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
 const cors = require('cor')
+
+if (process.env.NODE_ENV === 'local') {
+  app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+  }))
+} else {
+  app.use(cors({
+    credentials: true
+  }))
+}
+
