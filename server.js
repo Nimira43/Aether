edafc8a99f36dcfc827440fd07351380e7d5d3d2
Nimira43 <n.rabone72@aol.com>
2025-10-07 16:35:16 +0100

    Set up server

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv')
+const cors = requires('cors')
 
+if (process.env.NODE_ENV === 'local') {
+  app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+  }))
+} else {
+  app.use(cors({
+    credentials: true
+  }))
+}
+
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
