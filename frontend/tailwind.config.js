170dfb7044ac6ad17a14b4f2b3c650fb246dc22f
Nimira43 <n.rabone72@aol.com>
2025-10-07 17:07:32 +0100

    Set up Tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'main': '#ff4500',
+  			'support': '#008080',
+  			'light': '#fffcfa',
+  			'dark': '#111',
+  			'grey-dark': '#333',
+  			'grey-medium': '#898989',
+  			'grey-light': '#ccc',
+  			'grey-light-extra': '#eee',
+      },
+    },
+  },
+  plugins: [],
+}
+
