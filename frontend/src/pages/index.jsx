6dc6159cc4e4a646510762dec294d1b75bdde82a
Nimira43 <n.rabone72@aol.com>
2025-10-07 18:00:08 +0100

    Logo text

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -3,7 +3,11 @@ const Index = () => {
     <div className='bg-light min-h-screen w-full'>
       <div className='bg-main-light shadow-md'>
         <div className='w-[93%] m-auto py-3'>
-          
+          <div className='flex justify-between items-center'>
+            <div className='w-[80px] h-[40px]'>
+              <h1 className='logo text-dark text-4xl'>Aether</h1>
+            </div>
+          </div>
         </div>
       </div>
     </div>
