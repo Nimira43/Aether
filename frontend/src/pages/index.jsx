22a9cef669c7a4e2f3c666e23e20149fd63bfaaf
Nimira43 <n.rabone72@aol.com>
2025-10-07 19:44:09 +0100

    Login and Register buttons

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,11 +1,15 @@
 const Index = () => {
   return (
     <div className='bg-light min-h-screen w-full'>
-      <div className='bg-main-light shadow-md'>
+      <div className='bg-main shadow-md'>
         <div className='w-[93%] m-auto py-3'>
           <div className='flex justify-between items-center'>
             <div className='w-[80px] h-[40px]'>
-              <h1 className='logo text-dark text-4xl'>Aether</h1>
+              <h1 className='logo text-light text-4xl'>Aether</h1>
+            </div>
+            <div className='flex gap-4'>
+              <button className='main-btn'>Login</button>
+              <button className='main-btn'>Register</button>
             </div>
           </div>
         </div>
