978a337d5ff40a1b59ad9d76f0e42bbb301ea02d
Nimira43 <n.rabone72@aol.com>
2025-10-07 17:15:53 +0100

    Testing our customised styling - all good.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 const App = () => {
   return ( 
-    <div className='bg-black text-orange-400 text-4xl p-6'>Aether</div>
+    <div className='bg-dark text-main text-4xl logo p-6'>Aether</div>
   )
 }
  
