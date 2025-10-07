5d8fac9ac77a7e2d63b41fc1b42c3a4a991517af
Nimira43 <n.rabone72@aol.com>
2025-10-07 20:17:44 +0100

    Login / Register modal - 1

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -2,9 +2,20 @@ import { useState } from 'react'
 
 const Index = () => {
   const [show, setShow] = useState(false)
-  
+
   return (
     <div className='bg-light min-h-screen w-full'>
+      <div 
+        className={`
+          w-screen ${show 
+            ? 'visible opacity-30' 
+            : 'invisible opacity-30'
+          } 
+          transition-all duration-500 h-screen fixed bg-light flex justify-center items-center
+        `}
+      >
+
+      </div>
       <div className='bg-main shadow-md'>
         <div className='w-[93%] m-auto py-3'>
           <div className='flex justify-between items-center'>
@@ -20,8 +31,8 @@ const Index = () => {
       </div>
       <div className='w-full h-full justify-center items-center p-4'>
         <div className='py-[170px] flex justify-center items-center flex-col gap-6'>
-          <h2 className='logo text-main text-6xl font-semibold'>Where ideas awaken and colours speak</h2>
-          <span className='text-grey-dark font-medium text-xl'>Aether makes it easy to create and share professional designs.</span>
+          <h2 className='logo text-main text-6xl font-semibold text-center'>Where ideas awaken and colours speak</h2>
+          <span className='text-grey-dark font-medium text-xl text-center'>Aether makes it easy to create and share professional designs.</span>
           <button className='dark-btn'>Register</button>
         </div>
       </div>
