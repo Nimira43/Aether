5f4941e017cb73b03aebad681f0c31902c08ed37
Nimira43 <n.rabone72@aol.com>
2025-12-07 11:16:15 +0000

    Setting Home route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Index from './pages'
 import Layout from './pages/Layout'
+import Home from './components/Home'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Layout />
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      }
+    ]
   }
 ])
 
