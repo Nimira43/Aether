f844ffc2bdc68a5fbf126885a7282a747334480d
Nimira43 <n.rabone72@aol.com>
2025-12-26 12:27:37 +0000

    Button added.

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,6 +2,12 @@ const Home = () => {
   return ( 
     <div className='pt-1 pl-3'>
       <div className='w-full centre h-[250px] bg-main-medium relative rounded-md overflow-hidden'>
+        <button 
+          className='main-btn overflow-hidden absolute top-3 right-3'
+        >
+          Create
+        </button>
+        
         <div>
           <h2 className='text-3xl pb-10 pt-6 font-medium text-light'>What will you create today?</h2>
         </div>
