168520360e8f05f5b6b5445687bf9b016a6e7fa4
Nimira43 <n.rabone72@aol.com>
2025-12-26 15:42:29 +0000

    Removed unneeded markup

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <div className='pt-1 pl-3'>
       <div className='w-full centre h-[250px] bg-main-medium relative rounded overflow-hidden'>
         <button
-          onClick={() => setShow(show)}  
+          onClick={() => setShow(!show)}  
           className='main-btn px-4 overflow-hidden absolute top-3 right-3'
         >
           Custom Size
@@ -35,21 +35,8 @@ const Home = () => {
                 id='width'
                 className='w-full px-2 py-[4px] input-field'
               />
-              <span className='text-grey-1'>victoria@user.com</span>
             </div>
           </div>
-          <ul className='text-grey'>
-            <li>
-              <a className='p-2 cursor-pointer'>
-                <span>Settings</span>
-              </a>
-            </li>
-            <li>
-              <a className='p-2 cursor-pointer'>
-                <span>Logout</span>
-              </a>
-            </li>
-          </ul>
         </form>        
         <div>
           <h2 className='text-3xl pb-10 pt-6 font-medium text-light'>What will you create today?</h2>
