561f23e2019fe13b027944d16b3904b6eeeee252
Nimira43 <n.rabone72@aol.com>
2026-05-12 17:50:45 +0100

    Expanded CreateComponent.

--- frontend/src/components/CreateComponent.jsx
+++ frontend/src/components/CreateComponent.jsx
@@ -3,7 +3,7 @@ const CreateComponent = ({
   currentComponent,
   removeComponent
 }) => {
-
+  const randValue = Math.floor(Math.random() * 100)
   let html = ''
 
   if (info.name === 'main_frame') {
@@ -30,6 +30,31 @@ const CreateComponent = ({
       </div> 
   }
   
+  if (info.name === 'shape' && info.type === 'rect') {
+    html = (
+      <div
+        id={randValue}
+        onClick={() => info.setCurrentComponent(info)}
+        style={{
+          width: info.width + 'px',
+          height: info.height + 'px',
+          background: info.colour,
+          opacity: info.opacity,
+          left: info.left + 'px',
+          top: info.top + 'px',
+          zIndex: info.z_index,
+          transform: info.rotate
+            ? `rotate(${info.rotate}deg)`
+            : 'rotate(0deg)'
+        }}
+        className='absolute group hover:border-[2px] hover:border-main'
+
+      >
+
+      </div>
+    )
+  } 
+
   return html
 }
  
